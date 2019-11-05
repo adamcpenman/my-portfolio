89ca0a357a4ed03ce899b7b87afd421499531dc0
Your Name <you@example.com>
2019-11-04 19:51:36 -0600

    welcome message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Adam's Portfolio...
+          Welcome to Adam's Basic Portfolio...
         </p>
        
       </header>
